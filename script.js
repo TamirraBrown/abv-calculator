2cf15c92c4e331f13efd82851075be795cdef9cc
TamirraBrown <108706703+TamirraBrown@users.noreply.github.com>
2023-05-09 17:00:07 -0400

    update homepage with image

--- script.js
+++ script.js
@@ -11,15 +11,6 @@ function calculateABV() {
     document.getElementById("abv").innerHTML = abv;
   }
 
-  function searchByKeyword() {
-    var results = YouTube.Search.list('id,snippet', {q: 'dogs', maxResults: 25});
-  
-    for(var i in results.items) {
-      var item = results.items[i];
-      Logger.log('[%s] Title: %s', item.id.videoId, item.snippet.title);
-    }
-  }
-
 
   //YouTube iFrame
 
